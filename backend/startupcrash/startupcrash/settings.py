56215b2ae6d29585ca1b9e367ad8b47e2d9e08dc
Roc7-gif <zosroc@gmail.com>
2026-03-11 19:14:09 +0100

    commit

--- backend/startupcrash/startupcrash/settings.py
+++ backend/startupcrash/startupcrash/settings.py
@@ -24,7 +24,7 @@ TEMPLATES_DIR = os.path.join (BASE_DIR, 'template')
 SECRET_KEY = 'django-insecure-*$**z=s+-3sptkfi!ry22nlj8#p01@95y6#f%d_-5jzer6d7hp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1' , "localhost" , 'startupcrashlab.pythonanywhere.com' , "startupcrushlab.com"]
 
